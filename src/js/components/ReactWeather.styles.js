cf980c19dd0335c4de5c2e3979d53ff49d22065c
pongjoi <pongjoi@gmail.com>
2022-08-31 03:10:42 +0200

    Update ReactWeather.styles.js

--- src/js/components/ReactWeather.styles.js
+++ src/js/components/ReactWeather.styles.js
@@ -26,14 +26,52 @@ export const StyledContainer = styled.div`
 
   .rw-container-left {
     padding: 25px;
-    width: 60%;
+    width: 75%;
   }
 
   .rw-container-right {
     background-color: rgba(0, 0, 0, 0.1);
-    width: 40%;
+    width: 25%;
     display: flex;
     align-items: center;
     justify-content: center;
   }
+
+  .rw-today-date {
+    color: ${({ theme }) => theme.todayDateFontColor};
+    font-size: 16px;
+    margin: 0 0 10px 0;
+  }
+
+  .rw-today-hr {
+    width: 10px;
+    height: 80px;
+    border-left: solid 1px #fff;
+    opacity: 0.4;
+    margin: 0px 10px 0px 20px;
+  }
+
+  .rw-today-current {
+    font-size: 45px;
+    color: ${({ theme }) => theme.todayTempFontColor};
+  }
+
+  .rw-today-range {
+    color: ${({ theme }) => theme.todayRangeFontColor};
+    font-size: 14px;
+    margin: 0 0 5px 2px;
+  }
+
+  .rw-today-desc {
+    color: ${({ theme }) => theme.todayDescFontColor};
+    font-size: 16px;
+    margin-top: 16px;
+  }
+
+  .rw-today-info {
+    color: ${({ theme }) => theme.todayInfoFontColor};
+    font-size: 16px;
+  }
+
+  
 `;
